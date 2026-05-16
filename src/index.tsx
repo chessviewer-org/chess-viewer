d751c08297ba851aeec611efa56e5a7f6b1d4baf
Khatai Huseynzada <huseynzadekhatai@gmail.com>
2026-05-16 22:36:39 +0400

    feat: wrap App component with AuthProvider for enhanced authentication context

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,19 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import { AuthProvider } from '@/features/auth/hooks/useAuth';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
