8139e4e5be5c3d7244adb3cd415bb654587b42ef
Khatai Huseynzada <huseynzadekhatai@gmail.com>
2026-05-22 10:00:09 +0400

    fix: update import path for useFENBatch to use alias

--- src/components/features/Fen/FENInputField/FENInputField.tsx
+++ src/components/features/Fen/FENInputField/FENInputField.tsx
@@ -11,7 +11,7 @@ import {
 } from 'lucide-react';
 
 import ClipboardHistory from '../../ClipboardHistory';
-import { useFENBatch } from '@contexts';
+import { useFENBatch } from '@/contexts';
 import { useDebouncedFENValidation } from '@hooks/useDebouncedFENValidation';
 import { validateFEN } from '@utils';
 import { logger } from '@utils/logger';
