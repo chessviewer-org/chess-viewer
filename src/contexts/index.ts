bbfd1545ffbebfa74d7e566aa50a49cef871fc5c
BilgeGates <huseynzadekhatai@gmail.com>
2026-05-29 14:18:20 +0400

    refactor: remove ThemeSettingsProvider and Input component exports

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,4 +1,3 @@
-export { ThemeSettingsProvider } from './ThemeSettingsContext';
 export { FENBatchProvider } from './FENBatchContext';
 export { useFENBatch } from './useFENBatch';
 export { ModalProvider, useModal } from './ModalContext';
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,7 +3,6 @@ export { default as Checkbox } from './Checkbox/Checkbox';
 export { default as CustomSelect } from './CustomSelect/CustomSelect';
 export { default as DatePicker } from './DatePicker/DatePicker';
 export { ErrorBoundary } from './ErrorBoundary/ErrorBoundary';
-export { default as Input } from './Input/Input';
 export { default as Modal } from './Modal/Modal';
 export { default as ModalShell } from './Modal/ModalShell';
 export { default as NotificationContainer } from './NotificationContainer/NotificationContainer';
