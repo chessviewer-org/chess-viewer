72470ab60adbff08a7f63f95d53f1e428b6a2028
BilgeGates <huseynzadekhatai@gmail.com>
2026-06-01 14:23:40 +0400

    chore: remove ColorPicker and Export components

--- src/components/features/ColorPicker/views/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ThemeMainView } from './ThemeMainView';
-export { default as ThemeAdvancedPickerView } from './ThemeAdvancedPickerView';
-export { default as ThemeSettingsView } from './ThemeSettingsView';
