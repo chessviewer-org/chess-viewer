53701c5de42d0cea3f1efabc1946ae68e45adaec
BilgeGates <huseynzadekhatai@gmail.com>
2026-06-14 23:21:26 +0400

    refactor(settings): update barrel exports

--- src/pages/settings/index.ts
+++ src/pages/settings/index.ts
@@ -1,7 +1,5 @@
 export { default as AccountSection } from './AccountSection';
+export { default as AppearanceSection } from './AppearanceSection';
+export { default as BoardSection } from './BoardSection';
 export { default as DataManagement } from './DataManagement';
 export { default as SecuritySection } from './SecuritySection';
-export {
-  type SettingsSection,
-  default as SettingsSidebar
-} from './SettingsSidebar';
