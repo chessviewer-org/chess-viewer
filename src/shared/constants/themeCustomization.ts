59cd438117dc1301b400ad8f299883c9477febcf
BilgeGates <huseynzadekhatai@gmail.com>
2026-06-20 02:13:14 +0400

    refactor: remove accent-theme hooks and constants

--- src/shared/constants/themeCustomization.ts
+++
@@ -1,6 +0,0 @@
-export const STORAGE_KEYS = {
-  PRESETS: 'chess-diagram-presets',
-  SETTINGS: 'chess-diagram-settings',
-  LIGHT_SQUARE: 'chess-light-square',
-  DARK_SQUARE: 'chess-dark-square'
-} as const;
