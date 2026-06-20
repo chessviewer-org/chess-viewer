16e6163c0fd383ca468aa16eda57599499145154
BilgeGates <huseynzadekhatai@gmail.com>
2026-06-20 08:37:45 +0400

    chore(constants): drop themeCustomization and accentThemes from barrel

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,6 +1,4 @@
-export * from './accentThemes';
 export * from './chessConstants';
 export * from './dragDropConstants';
 export * from './seoConstants';
 export * from './syncConstants';
-export * from './themeCustomization';
